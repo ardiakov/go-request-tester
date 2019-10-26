e780393db406fc478fb67c3cabb5b68f59ecc25e
ardiakov <adiakov.work@gmail.com>
2019-10-26 23:47:08 +0300

    Добавил работу с каналами

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 type Config struct {
@@ -16,20 +15,17 @@ type Config struct {
 func main() {
 	config := Config{"https://openinnovations.ru/api/map", 100, 4}
 
-	counter := 0
+	channel := make(chan int)
 
 	for i := 1; i <= config.Concurrency; i++ {
-		go sendRequest(i, config.CountRequests, config.Url, &counter)
+		go sendRequest(i, config.CountRequests, config.Url, channel)
 	}
 
-	time.Sleep(time.Second * 10)
-
-	fmt.Printf("Всего запросов: %d", counter)
+	fmt.Printf("Всего запросов: %d", <-channel)
 }
 
-func sendRequest(Concurrency int, CountRequests int, Url string, counter *int) {
+func sendRequest(Concurrency int, CountRequests int, Url string, channel chan int) {
 	for i := 1; i <= CountRequests; i++ {
-		*counter++
 
 		response, _ := http.Get(Url)
 
@@ -38,7 +34,9 @@ func sendRequest(Concurrency int, CountRequests int, Url string, counter *int) {
 		if response.StatusCode != 200 {
 			log.Panic("Fuck")
 		}
-
 		fmt.Printf("Concurency: %d | Request: %d\n", Concurrency, i)
 	}
+
+	data := <-channel
+	channel <- data + CountRequests
 }
